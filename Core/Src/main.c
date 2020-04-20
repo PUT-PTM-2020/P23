7211e69c29a683b704d89aae857afeb5524930df
Maciej <48365963+Mchajda@users.noreply.github.com>
2020-04-20 19:35:15 +0200

    +

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -55,90 +55,83 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-char klawiatur(){
+uint8_t klawiter(){
 	while(1){
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 0);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, 1);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1);
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0) == 0){}
-		  return '1';
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, 0);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, 1);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 1);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, 1);
+		//1
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){}
+			return '1';
 		}
 
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1) == 0){}
-		  return '4';
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){}
+			return '2';
 		}
 
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2) == 0){}
-		  return '7';
-		}
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3) == 0){}
-		  return '*';
-		}
-
-		//2
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, 0);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 1);
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0) == 0){}
-		  return '2';
-		}
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1) == 0){}
-		  return '5';
-		}
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2) == 0){}
-		  return '8';
-		}
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3) == 0){
-
-		  while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3) == 0){}
-		  return '0';
-		}
-
-		//3
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, 1);
-		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, 1);
-		HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, 0);
-
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0) == 0){
-			while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_0) == 0){}
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){
+			while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){}
 			return '3';
 		}
 
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1) == 0){
-			while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_1) == 0){}
+		//2
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0, 1);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, 0);
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){}
+			return '4';
+		}
+
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){}
+			return '5';
+		}
+
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){
+			while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){}
 			return '6';
 		}
 
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2) == 0){
-			while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_2) == 0){}
+		//3
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_1, 1);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 0);
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){}
+			return '7';
+		}
+
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){}
+			return '8';
+		}
+
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){
+			while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){}
 			return '9';
 		}
 
-		if(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3) == 0){
-			while(HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_3) == 0){}
+		//4
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_2, 1);
+		HAL_GPIO_WritePin(GPIOD, GPIO_PIN_3, 0);
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_6)==0){}
+			return '*';
+		}
+
+		if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){
+			while(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_8)==0){}
+			return '0';
+		}
+
+		if(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){
+			while(HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10)==0){}
 			return '#';
 		}
 	}
+
 }
 /* USER CODE END 0 */
 
@@ -171,15 +164,14 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  char zwrot;
+  uint8_t zwrot;
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-
-	  zwrot = klawiatur();
+	  zwrot = klawiter();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -240,31 +232,34 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2 
+                          |GPIO_PIN_3, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PC6 PC8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_8;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PD0 PD1 PD2 PD3 */
-  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3;
+  /*Configure GPIO pins : PD15 PD0 PD1 PD2 
+                           PD3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_15|GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2 
+                          |GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC6 PC8 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
